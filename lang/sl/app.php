ca9195c105991a7280f1b5697164fe1b30e8d067
Marko Kodrič <marko.kodric@spletna-postaja.com>
2020-10-01 12:21:19 +0200

    Added slovenian translations

---
+++ lang/sl/app.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'name' => 'Uptime Robot',
+    'description' => 'Ponuja gradnike z Uptime Robot statistiko za nadzorno ploščo.',
+    'settings' => 'Upravljanje nastavitev povezanih z vtičnikom Uptime Robot.',
+    'permission' => [
+        'view_widgets' => 'Prikaz gradnikov na nadzorni plošči.',
+        'access_settings' => 'Upravljanje nastavitev',
+    ],
+    'widget' => [
+        'overall_uptime' => 'Skupni čas delovanja',
+        'response_time' => 'Odzivni čas',
+    ],
+];
---
+++ lang/sl/strings.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'past_days' => '{1} v preteklem dnevu|[2,Inf] v preteklih :value dneh',
+];
